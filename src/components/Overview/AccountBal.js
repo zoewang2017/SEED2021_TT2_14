ec77571da3b7e81e40311f34cbacd9f91019f1a9
zoewang2017 <zoe.wang.2017@sis.smu.edu.sg>
2020-12-11 16:08:13 +0800

    Update AccountBal.js

--- src/components/Overview/AccountBal.js
+++ src/components/Overview/AccountBal.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
 
 import { axiosInstance as axiosAuth } from "../../utils/API_Instance";
 
@@ -32,11 +33,60 @@ export default function AccountBal() {
       });
   };
 
+  //   const [error, setError] = useState(null);
+  //   const [isLoaded, setIsLoaded] = useState(false);
+  //   const [items, setItems] = useState([]);
+
+  //   // Note: the empty deps array [] means
+  //   // this useEffect will run once
+  //   // similar to componentDidMount()
+  //   useEffect(() => {
+  //     fetch("https://api.example.com/items")
+  //       .then((res) => res.json())
+  //       .then(
+  //         (result) => {
+  //           setIsLoaded(true);
+  //           setItems(result);
+  //         },
+  //         // Note: it's important to handle errors here
+  //         // instead of a catch() block so that we don't swallow
+  //         // exceptions from actual bugs in components.
+  //         (error) => {
+  //           setIsLoaded(true);
+  //           setError(error);
+  //         }
+  //       );
+  //   }, []);
+
   return (
     <div>
-      <Typography variant="h3">Welcome back, Name</Typography>
-      <br />
-      <Typography variant="subtitle">Your account balance: </Typography>
+      <Grid container>
+        <Grid item xs={12}>
+          Welcome Back!
+        </Grid>
+        <Grid item xs={12}>
+          Your account balances{" "}
+        </Grid>
+        <Grid item xs={6}>
+          Savings Account:{" "}
+        </Grid>
+        <Grid item xs={6}>
+          0
+        </Grid>
+        <Grid item xs={6}>
+          Multiplier Account:{" "}
+        </Grid>
+        <Grid item xs={6}>
+          374.15
+        </Grid>
+        <Grid item xs={6}>
+          Current Account:{" "}
+        </Grid>
+        <Grid item xs={6}>
+          414.9
+        </Grid>
+      </Grid>
+
       <Link onClick={() => handleSignUp()}> click here</Link>
       <br />
       <Link onClick={() => handleSignUp()}> View recent transactions</Link>

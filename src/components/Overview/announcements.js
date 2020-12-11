68dc2cecb09038b033d15b298472aaea470eabee
Puteri16 <48663544+Puteri16@users.noreply.github.com>
2020-12-11 16:11:37 +0800

    Add banner

--- src/components/Overview/announcements.js
+++ src/components/Overview/announcements.js
@@ -19,28 +19,19 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 const tutorialSteps = [
   {
     label: "Forget your wallet?",
-    imgPath:"../public/banner1.png",
+    imgPath:"https://i.ibb.co/kQDswYh/banner1.png",
   },
   {
     label: "Bird",
     imgPath:
-      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+      "https://i.ibb.co/DQwrw3w/WF.png",
   },
   {
     label: "Bali, Indonesia",
     imgPath:
-      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80",
+      "https://i.ibb.co/PWxtfzN/ewallet.png",
   },
-  {
-    label: "NeONBRAND Digital Marketing, Las Vegas, United States",
-    imgPath:
-      "https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-  {
-    label: "Goč, Serbia",
-    imgPath:
-      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
-  },
+
 ];
 
 const useStyles = makeStyles((theme) => ({
